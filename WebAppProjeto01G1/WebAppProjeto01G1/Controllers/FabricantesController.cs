1786baedb7054c21fe9b5896880b58a51632f7be
George Azevedo da Silva <gasilvabr@hotmail.com>
2022-06-09 10:22:07 -0300

    Inclusao do Delete

--- WebAppProjeto01G1/WebAppProjeto01G1/Controllers/FabricantesController.cs
+++ WebAppProjeto01G1/WebAppProjeto01G1/Controllers/FabricantesController.cs
@@ -97,5 +97,38 @@ namespace WebAppProjeto01G1.Controllers
             }
             return View(fabricante);
         }
+
+
+        // GET: Fabricantes/Delete/5
+        [HttpGet]
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Fabricante fabricante = context.Fabricantes.Find(id);
+            Fabricante fabricante = fabricantes.Where(m => m.FabricanteId == id).First();
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        // POST: Fabricantes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Fabricante fabricante = fabricantes.Where(c => c.FabricanteId == id).First();
+            fabricantes.Remove(fabricante);
+            //Fabricante fabricante = context.Fabricantes.Find(id);
+            //context.Fabricantes.Remove(fabricante);
+            //context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
