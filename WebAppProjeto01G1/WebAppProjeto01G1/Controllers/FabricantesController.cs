ae2503ad225e1829f700bcd1356ce9b2d6891ecf
George Azevedo da Silva <gasilvabr@hotmail.com>
2022-08-11 10:45:59 -0300

    Inclusao da lista de produtos por fabricante

--- WebAppProjeto01G1/WebAppProjeto01G1/Controllers/FabricantesController.cs
+++ WebAppProjeto01G1/WebAppProjeto01G1/Controllers/FabricantesController.cs
@@ -89,8 +89,9 @@ namespace WebAppProjeto01G1.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Fabricante fabricante = context.Fabricantes.Find(id);
+            //Fabricante fabricante = context.Fabricantes.Find(id);
             //Fabricante fabricante = fabricantes.Where(m => m.FabricanteId == id).First();
+            Fabricante fabricante = context.Fabricantes.Where(f =>f.FabricanteId == id).Include("Produtos.Categoria").First();
             if (fabricante == null)
             {
                 return HttpNotFound();
