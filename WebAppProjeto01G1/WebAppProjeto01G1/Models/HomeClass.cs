5fdc7ab54b3daf562481c1e8303079c8f5631c76
George Azevedo da Silva <gasilvabr@hotmail.com>
2022-12-05 11:14:06 -0300

    Incluindo controlador Home

---
+++ WebAppProjeto01G1/WebAppProjeto01G1/Models/HomeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto01G1.Models
+{
+    public class HomeClass
+    {
+        List<Modelo.Cadastros.Produto> listaProdutoLancamento;
+        List<Modelo.Cadastros.Produto> listaprodutoDestaques;
+    }
+}
