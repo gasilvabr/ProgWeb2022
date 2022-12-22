c4eb4340c00173aaf4b5cf832afb82352f48c3d1
George Azevedo da Silva <gasilvabr@hotmail.com>
2022-12-22 10:40:46 -0300

    Autenticacao G1

---
+++ WebAppProjeto01G1/WebAppProjeto01G1/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto01G1.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb")
+        { 
+        }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+        public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>
+        { 
+        }
+    }
+}
