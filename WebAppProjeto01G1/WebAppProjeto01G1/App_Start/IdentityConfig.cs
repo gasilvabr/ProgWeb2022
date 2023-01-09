8b402f0afcef0f84a59e6a4561a7f5c203ab8f93
George Azevedo da Silva <gasilvabr@hotmail.com>
2023-01-09 08:40:28 -0300

    Usuario G1

---
+++ WebAppProjeto01G1/WebAppProjeto01G1/App_Start/IdentityConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppProjeto01G1.DAL;
+using WebAppProjeto01G1.Infraestrutura;
+
+namespace WebAppProjeto01G1
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Seguranca/Account/Login"),
+            });
+        }
+    }
+}
